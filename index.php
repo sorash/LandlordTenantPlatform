5b8e7d8147db7d68bea769527ac56ef5558c8855
sorash <ashrafisoroush@gmail.com>
2017-02-22 00:53:47 -0500

    added main page styling and test examples

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Landlord/Tenant Platform</title>
+	<link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/sitecss.css"   type="text/css" media="screen" />
+  </head>
+
+  <body>
+    <section>
+	  <div class="content">
+		<div id="issuesList">
+		  <div class="issue">
+		    <table>
+			  <col width="80%">
+			  
+			  <tr>
+			    <th>Issue Title</th>
+				<td>Issued By: user_name</td>
+			  </tr>
+			  
+			  <tr>
+			    <td class="description">
+				Lorem ipsum dolor sit amet, pri eius alienum at. Duo ei affert putent facete, possit accusam quo id. Ei nec sale nullam interpretaris, vel id euismod mandamus salutandi, cu usu fugit ornatus signiferumque. Ea has habeo democritum. Sed ea adipisci vulputate comprehensam, graecis ancillae accusata vel ea.
+
+				Te nulla decore nec, at illum summo adipisci pri. Verterem consequat pro ad. Rebum ipsum ei mel, pri cu partem tempor. Tractatos ullamcorper vis ex, volumus incorrupte vel ei.
+
+				Est et decore consul eripuit, id impedit menandri has. Erant propriae nominavi te eum, an est mollis placerat electram. Integre facilisis pri ne, tale aperiri ea vel. Ea duo tation deterruisset, natum discere pri at.
+
+				Illum ignota est et. At vel placerat facilisi suavitate, lucilius interesset instructior per ea. Eum quidam consulatu et. Everti mediocrem similique et nam, cum cu wisi numquam sententiae. Tota prompta gubergren cum ex.
+
+				Legimus mnesarchum sea at, mea blandit eleifend percipitur cu. Nemore pericula id vix, ea qui nisl nulla menandri. Eos no malis sanctus percipitur, ad detraxit explicari torquatos per. Verear assentior pro at. Noster labitur reprimique eu ius, id nec sumo constituam, putant tamquam in has.
+				</td>
+				
+				<td>Issued On: date</td>
+			  </tr>
+			</table>
+		  </div>
+		  
+		  <div class="issue">
+		    <table>
+			  <col width="80%">
+			  
+			  <tr>
+			    <th>Issue Title</th>
+				<td>Issued By: user_name</td>
+			  </tr>
+			  
+			  <tr>
+			    <td class="description">
+				Lorem ipsum dolor sit amet, pri eius alienum at. Duo ei affert putent facete, possit accusam quo id. Ei nec sale nullam interpretaris, vel id euismod mandamus salutandi, cu usu fugit ornatus signiferumque. Ea has habeo democritum. Sed ea adipisci vulputate comprehensam, graecis ancillae accusata vel ea.
+
+				Te nulla decore nec, at illum summo adipisci pri. Verterem consequat pro ad. Rebum ipsum ei mel, pri cu partem tempor. Tractatos ullamcorper vis ex, volumus incorrupte vel ei.
+
+				Est et decore consul eripuit, id impedit menandri has. Erant propriae nominavi te eum, an est mollis placerat electram. Integre facilisis pri ne, tale aperiri ea vel. Ea duo tation deterruisset, natum discere pri at.
+
+				Illum ignota est et. At vel placerat facilisi suavitate, lucilius interesset instructior per ea. Eum quidam consulatu et. Everti mediocrem similique et nam, cum cu wisi numquam sententiae. Tota prompta gubergren cum ex.
+
+				Legimus mnesarchum sea at, mea blandit eleifend percipitur cu. Nemore pericula id vix, ea qui nisl nulla menandri. Eos no malis sanctus percipitur, ad detraxit explicari torquatos per. Verear assentior pro at. Noster labitur reprimique eu ius, id nec sumo constituam, putant tamquam in has.
+				</td>
+				
+				<td>Issued On: date</td>
+			  </tr>
+			</table>
+		  </div>
+		  
+		  <div class="issue">
+		    <table>
+			  <col width="80%">
+			  
+			  <tr>
+			    <th>Issue Title</th>
+				<td>Issued By: user_name</td>
+			  </tr>
+			  
+			  <tr>
+			    <td class="description">
+				Lorem ipsum dolor sit amet, pri eius alienum at. Duo ei affert putent facete, possit accusam quo id. Ei nec sale nullam interpretaris, vel id euismod mandamus salutandi, cu usu fugit ornatus signiferumque. Ea has habeo democritum. Sed ea adipisci vulputate comprehensam, graecis ancillae accusata vel ea.
+
+				Te nulla decore nec, at illum summo adipisci pri. Verterem consequat pro ad. Rebum ipsum ei mel, pri cu partem tempor. Tractatos ullamcorper vis ex, volumus incorrupte vel ei.
+
+				Est et decore consul eripuit, id impedit menandri has. Erant propriae nominavi te eum, an est mollis placerat electram. Integre facilisis pri ne, tale aperiri ea vel. Ea duo tation deterruisset, natum discere pri at.
+
+				Illum ignota est et. At vel placerat facilisi suavitate, lucilius interesset instructior per ea. Eum quidam consulatu et. Everti mediocrem similique et nam, cum cu wisi numquam sententiae. Tota prompta gubergren cum ex.
+
+				Legimus mnesarchum sea at, mea blandit eleifend percipitur cu. Nemore pericula id vix, ea qui nisl nulla menandri. Eos no malis sanctus percipitur, ad detraxit explicari torquatos per. Verear assentior pro at. Noster labitur reprimique eu ius, id nec sumo constituam, putant tamquam in has.
+				</td>
+				
+				<td>Issued On: date</td>
+			  </tr>
+			</table>
+		  </div>
+		</div>
+	  </div>	  
+	</section>
+	
+	<aside>
+	  <div class="content">
+	  	<p>Rent Amount:</p>
+		<p id="rentAmount">rent_amount</p>
+		
+		<hr />
+		
+		<p>Address:</p>
+		<p id="address">address</p>
+		
+		<hr />
+		<p>Landlord Information</p>
+		<p id="landlordInfo">landlord_info</p>
+	  </div>
+	</aside>
+  </body>
+</html>
