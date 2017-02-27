b3fbc3f137a01e3f7b88ce71f6db64f64196cdc1
sorash <ashrafisoroush@gmail.com>
2017-02-26 22:29:08 -0500

    revamped welcome page

--- welcome.php
+++ welcome.php
@@ -1,36 +1,3 @@
-<?php
-    ob_start();
-    session_start();
-    require 'DBC.php';
-    
-    
-    if($_POST){
-        if($_POST["login"])
-        {
-            $db = new DBLink;
-            /*if(($row = $db->login($_POST)) == false){
-                echo "<p style='color:red;'>NO</p>";
-            }else 
-                echo "<p style='color:red;'>YES</p>";
-                var_dump($row);*/
-            if(($row = $db->login($_POST)) != false)
-            {
-
-                $_SESSION["LTPUserName"] = $row["FirstName"];
-                header("Location: index.php");
-                exit();
-            }else {
-                echo "<p style='color:red;font-size:20px;'> NOOOOOO</p>";
-            }
-        }
-        else if($_POST["register"])
-        {
-            $db = new DBLink;
-            $db->addUser($_POST);
-        }            
-    }    
-?>
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -41,77 +8,44 @@
   </head>
 
   <body>
-    <div class="parent">
-      <div class="half">
-        <form method="post" action="">
-          <div class="register">
-            <label>First Name:</label>
-            <input type="text" placeholder="Enter First Name" name="firstName" id="firstName" data-validation="custom" data-validation-regexp="^([a-zA-Z]+)$" required>
-	
-            <br /><br />
-   
-            <label>Last Name:</label>
-            <input type="text" placeholder="Enter Last Name" name="lastName" id="lastName"   data-validation="custom" data-validation-regexp="^([a-zA-Z]+)$" required>
-		  
-            <br /><br />
-    
-            <label>Email:</label>
-            <input type="email" placeholder="Enter Email" name="email" id="email" data-validation="email" required>
-		
-            <br /><br />
-    
-            <label>Password:</label>
-	          <input type="password" placeholder="Enter Password" name="pass_confirmation" data-validation="strength" data-validation-strength="2" type="password" >
-		
-            <br /><br />
-    
-            <label>Password (Confirmation):</label>
-            <input type="password" placeholder="Enter Password Again" name="pass" data-validation="confirmation"  type="password" > 
-		
-            <br /><br />
-    
-            <label>I am:</label>
-            <input type="radio" name="userType" value="landlord">Landlord
-            <input type="radio" name="userType" value="tenant">Tenant
-    
-            <br /><br />
-    
-            <div class="center">
-              <button type="submit" name="register">Register</button>
-		        </div>
-	        </div>
-	      </form>
-    </div><div class="half">
-      <form method="post" action="">
-          <div class="inner login">
-            <label>Email:</label>
-            <input type="text" placeholder="Enter Email" name="username" id="username" required>
+    <header>
+      <div id="header-inner">
+        <a id="logo"></a>
 
-		        <br /><br /><br />
-		
-            <label>Password:</label>
-            <input type="password" placeholder="Enter Password" name="password" id="password" required>
-		
-		        <br /><br />
-		
-		        <div class="center">
-              <button type="submit" name="login">Login</button>
-              <br /><br />
-              <input type="checkbox"> Remember Me
-		       </div>
-	      </div>
-	    </form>
+        <nav>
+          <ul>
+            <li><a href="https://github.com/sorash/LandlordTenantPlatform" target="_blank">Source Code</a></li>
+            <li><a href="http://civictech.ca/codeacross-toronto-2017/" target="_blank">CodeAcross Toronto 2017</a></li>
+
+            <li class="right">Already a member? <a href="login.php">Login</a></li>
+          </ul>
+        </nav>
       </div>
-    </div>  
-	  
-	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"> </script>
-    <script src="jQuery-Form-Validator/form-validator/jquery.form-validator.min.js"></script>
-    <script>
-         /* $.validate({
-            lang: 'en',
-            modules : 'security',
-            borderColorOnError : '#c90808'
-          });*/
-    </script>
+    </header>
+
+    <section class="inner-wrapper">
+      <article id="information">
+        <h1>Landlord/Tenant Platform</h1>
+        <h2>A place for landlords and tenants to connect and discuss issues regarding their properties.</h2>
+      </article>
+
+      <aside id="register">
+        <form method="POST">
+          <input type="text" placeholder="Enter First Name" name="firstName" id="firstName" data-validation="custom" data-validation-regexp="^([a-zA-Z]+)$" required>
+          <input type="text" placeholder="Enter Last Name" name="lastName" id="lastName"   data-validation="custom" data-validation-regexp="^([a-zA-Z]+)$" required>
+          <input type="email" placeholder="Enter Email" name="email" id="email" data-validation="email" required>
+          <input type="password" placeholder="Enter Password" name="pass_confirmation" data-validation="strength" data-validation-strength="2" required>
+          <input type="password" placeholder="Enter Password Again" name="pass" data-validation="confirmation"  type="password" required> 
+
+          <div id="userType">
+            <label>I am:</label>
+            <input type="radio" name="userType" id="landlord"><label for="landlord">Landlord</label>
+            <input type="radio" name="userType" id="tenant"><label for="tenant">Tenant</label>
+          </div>
+
+          <button type="submit">Sign Up</button>
+        </form>
+      </aside>
+    </section>
   </body>
 </html>
