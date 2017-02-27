bd7d93389cb60dda0229c66fd0115d2d4972f04f
sorash <ashrafisoroush@gmail.com>
2017-02-27 16:49:41 -0500

    added starting layout for revamped main page

--- index.php
+++ index.php
@@ -2,71 +2,49 @@
 <html>
   <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Landlord/Tenant Platform</title>
 	  <link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen" />
-	  <link rel="stylesheet" href="css/main.css"   type="text/css" media="screen" />
+	  <link rel="stylesheet" href="css/index.css"     type="text/css" media="screen" />
   </head>
 
   <body>
-    <nav>
-	    <ul>
-	      <li id="title">Landlord/Tenant Platform</li>
-		    <li id="account">user_name</li>
-	    </ul>
-	  </nav>
-	  
-    <section>	
-	    <div class="content">
-		    <div id="issuesList">
+    <header>
+      <div id="header-inner">
+        <a href="index.php" id="logo"></a>
+
+        <nav>
+          <a id="menu"></a>
+
+          <ul>
+            <li><a href="#">Create Issue</a></li>
+
+            <li class="right"><a href="logout.php">Logout</a></li>
+            <li class="right"><a href="profile.php">Profile</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <section class="inner-wrapper">
+      <article id="issue-list">
+        <div id="issue-list-inner">
+          <h1>Previous Issues</h1>
+
           <div class="issue">
             <table>
-              <col class="leftCol">
+              <tr class="issue-bar">
+                <td>#issue_id</td>
+                <td><a href="#"></a></td>
+              </tr>
 
               <tr>
-                <th>Issue Title</th>
+                <th>Issue Name</th>
                 <td>Issued By: user_name</td>
               </tr>
 
-              <tr class="bottomRow">
-                <td class="description">
-                Lorem ipsum dolor sit amet, pri eius alienum at. Duo ei affert putent facete, possit accusam quo id. Ei nec sale nullam interpretaris, vel id euismod mandamus salutandi, cu usu fugit ornatus signiferumque. Ea has habeo democritum. Sed ea adipisci vulputate comprehensam, graecis ancillae accusata vel ea.
-
-                Te nulla decore nec, at illum summo adipisci pri. Verterem consequat pro ad. Rebum ipsum ei mel, pri cu partem tempor. Tractatos ullamcorper vis ex, volumus incorrupte vel ei.
-
-                Est et decore consul eripuit, id impedit menandri has. Erant propriae nominavi te eum, an est mollis placerat electram. Integre facilisis pri ne, tale aperiri ea vel. Ea duo tation deterruisset, natum discere pri at.
-
-                Illum ignota est et. At vel placerat facilisi suavitate, lucilius interesset instructior per ea. Eum quidam consulatu et. Everti mediocrem similique et nam, cum cu wisi numquam sententiae. Tota prompta gubergren cum ex.
-
-                Legimus mnesarchum sea at, mea blandit eleifend percipitur cu. Nemore pericula id vix, ea qui nisl nulla menandri. Eos no malis sanctus percipitur, ad detraxit explicari torquatos per. Verear assentior pro at. Noster labitur reprimique eu ius, id nec sumo constituam, putant tamquam in has.
-                </td>
-
-                <td>Issued On: date</td>
-              </tr>
-            </table>
-		      </div>
-		  
-          <div class="issue">
-            <table>
-              <col class="leftCol">
-
               <tr>
-                <th>Issue Title</th>
-                <td>Issued By: user_name</td>
-              </tr>
-
-              <tr class="bottomRow">
-                <td class="description">
-                Lorem ipsum dolor sit amet, pri eius alienum at. Duo ei affert putent facete, possit accusam quo id. Ei nec sale nullam interpretaris, vel id euismod mandamus salutandi, cu usu fugit ornatus signiferumque. Ea has habeo democritum. Sed ea adipisci vulputate comprehensam, graecis ancillae accusata vel ea.
-
-                Te nulla decore nec, at illum summo adipisci pri. Verterem consequat pro ad. Rebum ipsum ei mel, pri cu partem tempor. Tractatos ullamcorper vis ex, volumus incorrupte vel ei.
-
-                Est et decore consul eripuit, id impedit menandri has. Erant propriae nominavi te eum, an est mollis placerat electram. Integre facilisis pri ne, tale aperiri ea vel. Ea duo tation deterruisset, natum discere pri at.
-
-                Illum ignota est et. At vel placerat facilisi suavitate, lucilius interesset instructior per ea. Eum quidam consulatu et. Everti mediocrem similique et nam, cum cu wisi numquam sententiae. Tota prompta gubergren cum ex.
-
-                Legimus mnesarchum sea at, mea blandit eleifend percipitur cu. Nemore pericula id vix, ea qui nisl nulla menandri. Eos no malis sanctus percipitur, ad detraxit explicari torquatos per. Verear assentior pro at. Noster labitur reprimique eu ius, id nec sumo constituam, putant tamquam in has.
-                </td>
-
+                <td>Issue Details</td>
                 <td>Issued On: date</td>
               </tr>
             </table>
@@ -74,49 +52,50 @@
 
           <div class="issue">
             <table>
-              <col class="leftCol">
+              <tr class="issue-bar">
+                <td>#issue_id</td>
+                <td><a href="#"></a></td>
+              </tr>
 
               <tr>
-                <th>Issue Title</th>
+                <th>Issue Name</th>
                 <td>Issued By: user_name</td>
               </tr>
 
-              <tr class="bottomRow">
-                <td class="description">
-                Lorem ipsum dolor sit amet, pri eius alienum at. Duo ei affert putent facete, possit accusam quo id. Ei nec sale nullam interpretaris, vel id euismod mandamus salutandi, cu usu fugit ornatus signiferumque. Ea has habeo democritum. Sed ea adipisci vulputate comprehensam, graecis ancillae accusata vel ea.
+              <tr>
+                <td>Issue Details</td>
+                <td>Issued On: date</td>
+              </tr>
+            </table>
+          </div>
 
-                Te nulla decore nec, at illum summo adipisci pri. Verterem consequat pro ad. Rebum ipsum ei mel, pri cu partem tempor. Tractatos ullamcorper vis ex, volumus incorrupte vel ei.
+          <div class="issue">
+            <table>
+              <tr class="issue-bar">
+                <td>#issue_id</td>
+                <td><a href="#"></a></td>
+              </tr>
 
-                Est et decore consul eripuit, id impedit menandri has. Erant propriae nominavi te eum, an est mollis placerat electram. Integre facilisis pri ne, tale aperiri ea vel. Ea duo tation deterruisset, natum discere pri at.
+              <tr>
+                <th>Issue Name</th>
+                <td>Issued By: user_name</td>
+              </tr>
 
-                Illum ignota est et. At vel placerat facilisi suavitate, lucilius interesset instructior per ea. Eum quidam consulatu et. Everti mediocrem similique et nam, cum cu wisi numquam sententiae. Tota prompta gubergren cum ex.
-
-                Legimus mnesarchum sea at, mea blandit eleifend percipitur cu. Nemore pericula id vix, ea qui nisl nulla menandri. Eos no malis sanctus percipitur, ad detraxit explicari torquatos per. Verear assentior pro at. Noster labitur reprimique eu ius, id nec sumo constituam, putant tamquam in has.
-                </td>
-
+              <tr>
+                <td>Issue Details</td>
                 <td>Issued On: date</td>
               </tr>
             </table>
           </div>
         </div>
-	    </div>	  
-	  </section>
-	
-	  <aside>
-	    <div class="content">
-	  	  <p>Rent Amount:</p>
-		    <p id="rentAmount">rent_amount</p>
-		
-		    <hr />
-		
-		    <p>Address:</p>
-		    <p id="address">address</p>
-		
-		    <hr />
-        
-		    <p>Landlord Information</p>
-		    <p id="landlordInfo">landlord_info</p>
-	    </div>
-	  </aside>
+      </article>
+
+      <aside id="information">
+        <div id="information-inner">
+          <h1>Property Information</h1>
+          <p>hello</p>
+        </div>
+      </aside>
+    </section>
   </body>
 </html>
