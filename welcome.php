29a220e3dc968acbde4d5ce56bf2d198cf47b336
sorash <ashrafisoroush@gmail.com>
2017-03-03 14:19:31 -0500

    put normalize back in

--- welcome.php
+++ welcome.php
@@ -18,7 +18,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Welcome to LTP</title>
-	 <!-- <link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen" />-->
+	  <link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen" />
 	  <link rel="stylesheet" href="css/welcome.css"   type="text/css" media="screen" />
   </head>
 
